45934f468a8ebc55b7f11327e652602fa7d31d66
Ajay Singh <ajay@mentormaya.com>
2023-05-24 16:11:02 +0545

    logger updated to create a log dir if not exist

--- utils/logger.py
+++ utils/logger.py
@@ -1,3 +1,4 @@
+import os
 import logging
 from utils.constants import config
 from datetime import datetime as dt
@@ -11,6 +12,10 @@ LOG_FORMAT = "{:<19s} [{:^12s}] [{:^6s}] {:s}"
 class Logger:
     def __init__(self) -> None:
         self.logfile = LOG_FILE
+        # Create the directory if it doesn't exist
+        if not os.path.exists(config['LOG_DIR']):
+            os.makedirs(config['LOG_DIR'])
+        # Open the file in append mode (creating it if it doesn't exist)
         with open(self.logfile, mode="a+") as file:
             file.close()
         self.log(msg="Application Log Initialized!")
