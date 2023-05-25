f4bf33d36c78f6d8b0270b0ca3375407c5ed499a
Ajay Singh <ajay@mentormaya.com>
2023-05-25 10:29:58 +0545

    env naming conflict removed

--- app/database/config.py
+++ app/database/config.py
@@ -3,12 +3,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
-if "sqlite:///" in config["DATABASE_URL"]:
-    engine = create_engine(
-        config["DATABASE_URL"], connect_args={"check_same_thread": False}
-    )
-else:
-    engine = create_engine(config["DATABASE_URL"])
+# if "sqlite" in config["DATABASE_URL"]:
+engine = create_engine(
+    config["DATABASE_URL"], connect_args={"check_same_thread": False}
+)
+# else:
+#     engine = create_engine(config["DATABASE_URL"])
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- utils/constants.py
+++ utils/constants.py
@@ -1,6 +1,10 @@
+import os
 from dotenv import dotenv_values
 
 secret_env_list = dotenv_values(".env.secret")
 shared_env_list = dotenv_values(".env.shared")
+
+# for github only
+github_env = os.getenv("DATABASE_URL")
 
 config = dict({**secret_env_list, **shared_env_list})
