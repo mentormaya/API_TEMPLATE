b9af157b1a33105750158a24310084fe4b157fbf
Ajay Singh <ajay@mentormaya.com>
2023-05-25 10:35:47 +0545

    default database url added for the test if not existed anywhere

--- utils/constants.py
+++ utils/constants.py
@@ -7,4 +7,8 @@ shared_env_list = dotenv_values(".env.shared")
 # for github only
 github_env = os.getenv("DATABASE_URL")
 
+
 config = dict({**secret_env_list, **shared_env_list})
+
+if "DATABASE_URL" not in config.keys():
+    config["DATABASE_URL"] = "sqlite:///app/database/mis_dev.db"
