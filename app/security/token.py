617d71c9b3911654f6189f2663d5fe24d4b16034
Ajay Singh <ajay@mentormaya.com>
2023-05-25 13:33:52 +0545

    jwt used

--- app/security/token.py
+++ app/security/token.py
@@ -1,17 +1,47 @@
-from utils.constants import config
-from fastapi import HTTPException
+from datetime import datetime, timedelta
+from typing import Any, Union
 from jose import jwt, JWTError
+from fastapi import HTTPException
+from utils.constants import config
+from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordBearer
 
 # JWT configuration
 security = OAuth2PasswordBearer(tokenUrl="auth/token")
 
+password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+config["REFRESH_TOKEN_EXPIRE_DAYS"] = (
+    config["REFRESH_TOKEN_EXPIRE_DAYS"] * 24 * 7
+)  # 7 days
+
+
+def create_access_token(subject: Union[str, Any], expires_delta: int = None) -> str:
+    if expires_delta is not None:
+        expires_delta = datetime.utcnow() + expires_delta
+    else:
+        expires_delta = datetime.utcnow() + timedelta(
+            minutes=config["ACCESS_TOKEN_EXPIRE_MINUTES"]
+        )
+
+    to_encode = {"exp": expires_delta, "sub": str(subject)}
+    encoded_jwt = jwt.encode(to_encode, config["JWT_SECRET_KEY"], config["ALGORITHM"])
+    return encoded_jwt
+
 
 def verify_token(token: str):
     try:
         payload = jwt.decode(
-            token, config["SECRET_KEY"], algorithms=[config["ALGORITHM"]]
+            token, config["JWT_SECRET_KEY"], algorithms=[config["ALGORITHM"]]
         )
         return payload
     except JWTError:
         raise HTTPException(status_code=401, detail="Invalid token")
+
+
+def hash_password(password: str) -> str:
+    return password_context.hash(password)
+
+
+def verify_password(password: str, hashed_pass: str) -> bool:
+    return password_context.verify(password, hashed_pass)
